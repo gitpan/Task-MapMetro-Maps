580a8052fd6f26cb77fe632863236aeac9c6c14e
Erik Carlsson <info@code301.com>
2015-01-05 15:14:46 +0000

    Import of CSSON/Task-MapMetro-Maps-0.1000.tar.gz from CPAN.
    gitpan-cpan-distribution: Task-MapMetro-Maps
    gitpan-cpan-version:      0.1000
    gitpan-cpan-path:         CSSON/Task-MapMetro-Maps-0.1000.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

---
+++ lib/Task/MapMetro/Maps.pm
@@ -0,0 +1,83 @@
+package Task::MapMetro::Maps;
+
+our $VERSION = '0.1000'; # VERSION
+
+1;
+
+# ABSTRACT: All Map::Metro maps
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::MapMetro::Maps - All Map::Metro maps
+
+=head1 VERSION
+
+version 0.1000
+
+=head1 SYNOPSIS
+
+    $ cpanm Task::MapMetro::Maps
+
+=head1 TASK CONTENTS
+
+=head2 Map::Metro
+
+=head3 L<Map::Metro> 0.2101
+
+=head2 Available maps
+
+=head3 L<Map::Metro::Plugin::Map::Amsterdam>
+
+=head3 L<Map::Metro::Plugin::Map::Athens>
+
+=head3 L<Map::Metro::Plugin::Map::Barcelona>
+
+=head3 L<Map::Metro::Plugin::Map::Berlin>
+
+=head3 L<Map::Metro::Plugin::Map::Copenhagen>
+
+=head3 L<Map::Metro::Plugin::Map::Gothenburg>
+
+=head3 L<Map::Metro::Plugin::Map::Helsinki>
+
+=head3 L<Map::Metro::Plugin::Map::Oslo>
+
+=head3 L<Map::Metro::Plugin::Map::Stockholm>
+
+=head1 SEE ALSO
+
+L<Map::Metro>
+
+L<Task::MapMetro::Dev>
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015 - Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Erik Carlsson <info@code301.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Erik Carlsson <info@code301.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
